69b245a3299ab832e8ddf2e6696d5e4dce4a74dd
fggggg06122001@gmail.comgit config --global user.name fggggg06122001 <ggggg06122001@gmail.com>
2018-11-13 18:00:05 -0800

    Weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { OpenWeatherMapModule } from 'ionic-openweathermap';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    OpenWeatherMapModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data: Observable<any>;
 
+  constructor(public navCtrl: NavController,  public httpClient: HttpClient) {
+   this.getData(null);
+  }
+
+  getData(event){
+    //comment
+    //var place = event.target.value;
+    //console.log(place);
+    this.data = this.httpClient.get("http://api.openweathermap.org/data/2.5/weather?q=" + "san francisco,us" + "&units=metric" + "&appid=44ae5b3958af46dd696552dcf64d9372");
+    this.data.subscribe(data => {
+      console.log('my data: ', data);
+      console.log(data.temp);
+      console.log(data.coord);
+    })
   }
 
 }
